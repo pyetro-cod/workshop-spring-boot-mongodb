b2c8bfb88f6746dbb87d4f8f73fd40eead8b66a4
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-09-10 21:18:39 -0300

    Criando entity POST com user ananhado

---
+++ src/main/java/com/devppp/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.devppp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devppp.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
